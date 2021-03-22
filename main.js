d145abfa47af90f325f64fca9d5050aea838d9e5
wdmatthews <wdmatthews3@gmail.com>
2021-03-22 14:55:50 -0500

    Initial design and upload.
    Might eventually move to Vue to improve the looks.

---
+++ main.js
@@ -0,0 +1,37 @@
+const skills = [
+  'C#',
+  'Unity',
+  'Node.js',
+  'MongoDB',
+  'Vue.js',
+  'Vuetify.js',
+  'jQuery',
+  'Bootstrap',
+  'PHP',
+  'MySQL',
+];
+
+const skillsSection = mq.one('#skills-list');
+skills.forEach(skill => {
+  const listItem = mq.new('li', skillsSection);
+  listItem.text(skill);
+});
+
+const projects = [
+  {
+    name: 'mini-query',
+    github: 'https://wdmatthews.github.io/mini-query/',
+    description: 'A lightweight script to shorten code related to DOM manipulation.',
+    skills: 'JavaScript',
+  },
+];
+const projectsSection = mq.one('#projects-list');
+projects.forEach(project => {
+  const projectCard = mq.new('div', projectsSection);
+  projectCard.addClass('project');
+  projectCard.html(`
+    <p class="project-name"><a href="${project.github}" target="_blank">${project.name}</a></p>
+    <p class="project-description">${project.description}</p>
+    <p class="project-skills">${project.skills}</p>
+  `);
+});
