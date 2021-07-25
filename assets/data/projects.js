96258182970e00d506c9e680f4ebad0fcfa92248
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2021-07-24 20:41:22 -0500

    Added time tools project

--- assets/data/projects.js
+++ assets/data/projects.js
@@ -143,4 +143,10 @@ export default [
     description: 'A 2048-style game about squaring numbers and solving puzzles for both desktop and mobile on Unity WebGL.',
     skills: ['C#', 'Unity'],
   },
+  {
+    name: 'time-tools',
+    github: 'https://wdmatthews.github.io/time-tools/',
+    description: 'A simple timer and stopwatch.',
+    skills: ['JavaScript'],
+  },
 ]
