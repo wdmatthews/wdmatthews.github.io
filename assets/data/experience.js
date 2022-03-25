ddf7acb0937045ead08c3aade22a2b613e423dfa
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2022-03-25 18:40:40 -0500

    Updated experience

--- assets/data/experience.js
+++ assets/data/experience.js
@@ -3,9 +3,17 @@ export default [
     title: 'Web Developer',
     company: 'Block Bins',
     type: 'Part-Time',
+    start: 'December 2021',
+    end: 'January 2022',
+    description: 'I worked on designing a website based on modern JavaScript frameworks.',
+  },
+  {
+    title: 'Web Developer',
+    company: 'Block Bins',
+    type: 'Part-Time',
     start: 'June 2021',
     end: 'August 2021',
-    description: 'I am working on designing a website based on modern JavaScript frameworks.',
+    description: 'I worked on designing a website based on modern JavaScript frameworks.',
   },
   {
     title: 'Web Developer',
